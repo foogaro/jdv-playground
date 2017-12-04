a21a194ae1cc1b960b5b2264ed03eaafa713f060
root <root@foogaro.localdomain>
2017-12-04 21:00:14 +0100

    Added source install-common.sh for installing the VDB

--- ocp/drivers/install.sh
+++ ocp/drivers/install.sh
@@ -14,3 +14,10 @@ install_modules ${injected_dir}/modules
 
 # configure the JDBC driver in standalone.xml.  Driver is named "derby"
 configure_drivers ${injected_dir}/install.properties
+
+source /usr/local/s2i/install-teiid-common.sh
+install_deployments ${injected_dir}/HORST.vdb
+
+echo "Adding the OData role to the user luigi..."
+/opt/eap/bin/add-user.sh -a --silent=true -u "luigi" -p "luigi.2017" -g "admin,connect,odata,user"
+echo "Added the OData role to the user luigi"
